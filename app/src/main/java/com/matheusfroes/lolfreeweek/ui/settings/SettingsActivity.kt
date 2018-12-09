9e24ec8500119b1d150727b2df3403c683d2f216
Matheus Fróes <matheusfroesmarques@gmail.com>
2018-12-09 14:15:33 -0200

    Settings fix

--- app/src/main/java/com/matheusfroes/lolfreeweek/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/matheusfroes/lolfreeweek/ui/settings/SettingsActivity.kt
@@ -89,7 +89,7 @@ class SettingsActivity : AppCompatPreferenceActivity() {
                     .withAboutSpecial1("RIOT LICENSE")
                     .withAboutSpecial1Description(getString(R.string.license_riot))
                     .withAboutSpecial2("ICON LICENSE")
-                    .withAboutSpecial2Description("<div>Icons made by <a href='http://www.freepik.com' title='Freepik'>Freepik</a> from <a href='http://www.flaticon.com' title='Flaticon'>www.flaticon.com</a> is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0' target='_blank'>CC 3.0 BY</a></div>")
+                    .withAboutSpecial2Description("Icons made by <a href='http://www.freepik.com' title='Freepik'>Freepik</a> from <a href='http://www.flaticon.com' title='Flaticon'>www.flaticon.com</a> is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0' target='_blank'>CC 3.0 BY</a>")
                     .withAboutDescription("<a href='https://github.com/froesmatheus'>Matheus Fróes</a>")
                     .withActivityTitle(getString(R.string.about_app))
                     .start(this)
